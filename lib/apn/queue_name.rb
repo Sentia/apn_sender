aaaacbdf960a1ebc5c92c92b3f41ab999896dd7e
Mario Visic <mario@mariovisic.com>
2012-05-02 15:57:05 +0800

    Allow the queue name to be set in a setting.

--- lib/apn/queue_name.rb
+++ lib/apn/queue_name.rb
@@ -1,4 +1,14 @@
-module APN  
-  # Change this to modify the queue from which notification jobs are pushed and pulled
-  QUEUE_NAME = :apple_push_notifications
+module APN
+  class Config
+    class << self
+      attr_accessor :queue_name
+
+      def queue_name_with_blank_check
+        queue_name_without_blank_check || raise("APN: No queue name set, please set APN::Config.queue_name in an initializer")
+      end
+
+      alias_method :queue_name_without_blank_check, :queue_name
+      alias_method :queue_name, :queue_name_with_blank_check
+    end
+  end
 end
